64febccfd4644cd88fdb89237458e0450446bca9
Xenofon Zinoviou <xzinoviou@gmail.com>
2021-05-02 08:28:52 +0300

    GameTest - added test for consecutive throws with no mark

--- src/test/java/com/xzinoviou/bowling/gaming/GameTest.java
+++ src/test/java/com/xzinoviou/bowling/gaming/GameTest.java
@@ -12,4 +12,24 @@ public class GameTest {
     game.add(5);
     assertEquals(5, game.score());
   }
+
+  @Test
+  public void testTwoThrowsNoMark() {
+    Game game = new Game();
+    game.add(5);
+    game.add(4);
+    assertEquals(9, game.score());
+  }
+
+  @Test
+  public void testFourThrowsNoMark() {
+    Game game = new Game();
+    game.add(5);
+    game.add(5);
+    game.add(7);
+    game.add(2);
+    assertEquals(18, game.score());
+    assertEquals(9, game.scoreForFrame(1));
+    assertEquals(18, game.scoreForFrame(2));
+  }
 }
