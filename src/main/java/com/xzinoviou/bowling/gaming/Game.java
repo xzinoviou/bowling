3c75ac1dee442d9147b37b50b027e2ca52a00682
Xenofon Zinoviou <xzinoviou@gmail.com>
2021-05-03 12:48:15 +0300

    Game - removed getCurrentFrame, GameTest - removed assertions with getCurrentFrame

--- src/main/java/com/xzinoviou/bowling/gaming/Game.java
+++ src/main/java/com/xzinoviou/bowling/gaming/Game.java
@@ -4,16 +4,12 @@ import com.xzinoviou.bowling.scoring.Scorer;
 
 public class Game {
 
-  private int itsCurrentFrame = 1;
+  private int itsCurrentFrame = 0;
   private boolean firstThrowInFrame = true;
   private Scorer itsScorer = new Scorer();
 
   public int score() {
     return scoreForFrame(itsCurrentFrame);
-  }
-
-  public int getCurrentFrame() {
-    return itsCurrentFrame;
   }
 
   public void add(int pins) {
@@ -22,30 +18,26 @@ public class Game {
   }
 
   private void adjustCurrentFrame(int pins) {
-    if (firstThrowInFrame) {
-
-      if (!adjustFrameForStrike(pins)) {
-        firstThrowInFrame = false;
-      }
-    } else {
-      firstThrowInFrame = true;
+    if (lastBallInFrame(pins)) {
       advanceFrame();
+    } else {
+      firstThrowInFrame = false;
     }
   }
 
-  public int scoreForFrame(int theFrame) {
-    return itsScorer.scoreForFrame(theFrame);
+  private boolean lastBallInFrame(int pins) {
+    return strike(pins) || !firstThrowInFrame;
+  }
+
+  private boolean strike(int pins) {
+    return (firstThrowInFrame && pins == 10);
   }
 
   private void advanceFrame() {
     itsCurrentFrame = Math.min(10, itsCurrentFrame + 1);
   }
 
-  private boolean adjustFrameForStrike(int pins) {
-    if (pins == 10) {
-      advanceFrame();
-      return true;
-    }
-    return false;
+  public int scoreForFrame(int theFrame) {
+    return itsScorer.scoreForFrame(theFrame);
   }
 }
--- src/test/java/com/xzinoviou/bowling/gaming/GameTest.java
+++ src/test/java/com/xzinoviou/bowling/gaming/GameTest.java
@@ -25,7 +25,6 @@ public class GameTest {
     game.add(5);
     game.add(4);
     assertEquals(9, game.score());
-    assertEquals(2, game.getCurrentFrame());
   }
 
   @Test
@@ -37,7 +36,6 @@ public class GameTest {
     assertEquals(18, game.score());
     assertEquals(9, game.scoreForFrame(1));
     assertEquals(18, game.scoreForFrame(2));
-    assertEquals(3, game.getCurrentFrame());
   }
 
   @Test
@@ -46,7 +44,6 @@ public class GameTest {
     game.add(7);
     game.add(3);
     assertEquals(13, game.scoreForFrame(1));
-    assertEquals(2, game.getCurrentFrame());
   }
 
 
@@ -59,7 +56,6 @@ public class GameTest {
     assertEquals(13, game.scoreForFrame(1));
     assertEquals(18, game.scoreForFrame(2));
     assertEquals(18, game.score());
-    assertEquals(3, game.getCurrentFrame());
   }
 
   @Test
@@ -69,7 +65,6 @@ public class GameTest {
     game.add(6);
     assertEquals(19, game.scoreForFrame(1));
     assertEquals(28, game.score());
-    assertEquals(3, game.getCurrentFrame());
   }
 
   @Test
@@ -79,7 +74,6 @@ public class GameTest {
       game.add(10);
     }
     assertEquals(300, game.score());
-    assertEquals(10, game.getCurrentFrame());
   }
 
   @Test
